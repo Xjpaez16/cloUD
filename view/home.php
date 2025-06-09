de6a60b117b57e4f77ca3b3c5f181f41cac165fd
Angel <angelsteven1245@gmail.com>
2025-06-09 11:30:20 -0500

    frontend principal view acabado y totalmente responsivo )00/M(

--- view/home.php
+++ view/home.php
@@ -10,13 +10,14 @@
     <link rel="stylesheet" href="<?= BASE_URL ?>public/css/home.css">
 </head>
 
+
+
 <body class="relative min-h-screen bg-cover bg-bottom bg-no-repeat text-white font-sans "
     style="background-image: url('<?= BASE_URL ?>public/img/udfondo.jpg');">
 
-
     <!-- Contenido principal -->
     <div class="relative z-10">
-        <?php require_once __DIR__ . '/layouts/header.php'; ?>
+        <?php require_once __DIR__ . '/layouts/nav.php'; ?>
         <!-- Mensaje principal -->
         <main class="text-center py-16 px-4">
             <h1 class="text-3xl lg:text-6xl md:text-5xl  font-bold mb-20 font-sans md:font-sans pb-20 mt-20">
@@ -26,42 +27,48 @@
             <!-- Íconos -->
             <div class="ease-out duration-300 grid grid-cols-1 sm:grid-cols-3 gap-5 mb-80 font-bold">
                 <div class="flex flex-col items-center">
-                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-2">Gestiona Materiales<br>Académicos</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-2">Gestiona
+                        Materiales<br>Académicos</p>
                     <a href="#">
-                        <img src="<?= BASE_URL ?>public/img/materials.png" class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
+                        <img src="<?= BASE_URL ?>public/img/materials.png"
+                            class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
                             alt="Materiales">
                     </a>
                 </div>
                 <div class="flex flex-col items-center">
                     <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-2">Gestiona Tu<br> Usuario</p>
                     <a href="#">
-                        <img src="<?= BASE_URL ?>public/img/users.png" class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
+                        <img src="<?= BASE_URL ?>public/img/users.png"
+                            class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
                             alt="Usuario">
                     </a>
                 </div>
                 <div class="flex flex-col items-center">
                     <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-8">Agenda Tutorías</p>
                     <a href="#">
-                        <img src="<?= BASE_URL ?>public/img/calendar.png" class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
+                        <img src="<?= BASE_URL ?>public/img/calendar.png"
+                            class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
                             alt="Tutorías">
                     </a>
                 </div>
             </div>
 
             <!-- Título de secciones -->
-            <h2 class="text-2xl font-semibold mb-6">
+            <h2 class="text-4xl font-semibold mb-6">
                 ¿En qué área necesitas ayuda?
             </h2>
+            <br>
 
             <!-- Tarjetas de áreas -->
-            <div class="font-bold font-sans grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 px-6 ">
+            <div class="font-bold font-sans grid grid-cols-1 xl:grid-cols-5 sm:grid-cols-3 md:grid-cols-3 gap-6 px-6 ">
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
                     <a href="#">
                         <img src="<?= BASE_URL ?>public/img/database.png" class="w-32 h-40 mx-auto mb-2"
                             alt="Bases de Datos">
                     </a>
-                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px]">Bases de Datos</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] sm:text-[30px]">Bases de Datos
+                    </p>
                 </div>
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
@@ -96,7 +103,7 @@
             </div>
         </main>
     </div>
-    
+
 </body>
 
 </html>
