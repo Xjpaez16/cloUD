422893d21f401de7f42d0d4b083010d76b6d010d
Xjpaez16 <juandavidpaezsierra@gmail.com>
2025-06-09 00:20:31 -0500

    Correccion bg, iconos ampliados,personalizacion nav,falta adaptarla a celular

--- view/home.php
+++ view/home.php
@@ -7,47 +7,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Tailwind CSS vía CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Hand+Precursive&family=Montserrat:wght@400;700&display=swap"
-        rel="stylesheet">
-
-
-
+    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/home.css">
 </head>
 
-<body class="relative min-h-screen bg-cover bg-center bg-no-repeat text-white"
-    style="background-image: url('<?= BASE_URL ?>public/img/udfondo.jpg'); background-position: center -200px;">
+<body class="relative min-h-screen bg-cover bg-bottom bg-no-repeat text-white font-sans "
+    style="background-image: url('<?= BASE_URL ?>public/img/udfondo.jpg');">
 
-    <!-- Capa morada translúcida -->
-    <div class="absolute inset-0 bg-purple-900/70 z-0"></div>
 
     <!-- Contenido principal -->
     <div class="relative z-10">
         <?php require_once __DIR__ . '/layouts/header.php'; ?>
         <!-- Mensaje principal -->
         <main class="text-center py-16 px-4">
-            <h1 class="lg:text-6xl text-4xl sm:text-5xl font-bold mb-12 font-sans md:font-sans">
+            <h1 class="text-3xl lg:text-6xl md:text-5xl  font-bold mb-20 font-sans md:font-sans pb-20 mt-20">
                 Aprende y convive con los compañeros de la UD
             </h1>
 
             <!-- Íconos -->
-            <div class="grid grid-cols-1 sm:grid-cols-3 gap-8 mb-16">
+            <div class="ease-out duration-300 grid grid-cols-1 sm:grid-cols-3 gap-5 mb-80 font-bold">
                 <div class="flex flex-col items-center">
-                    <p class="font-sans-serif md:font-sans-serif">Gestiona Materiales Académicos</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-2">Gestiona Materiales<br>Académicos</p>
                     <a href="#">
-                        <img src="<?= BASE_URL ?>public/img/materials.png" class="w-40 h-48 mb-2" alt="Materiales">
+                        <img src="<?= BASE_URL ?>public/img/materials.png" class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
+                            alt="Materiales">
                     </a>
                 </div>
                 <div class="flex flex-col items-center">
-                    <p>Gestiona Tu Usuario</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-2">Gestiona Tu<br> Usuario</p>
                     <a href="#">
-                        <img src="<?= BASE_URL ?>public/img/users.png" class="w-40 h-48 mb-2" alt="Usuario">
+                        <img src="<?= BASE_URL ?>public/img/users.png" class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
+                            alt="Usuario">
                     </a>
                 </div>
                 <div class="flex flex-col items-center">
-                    <p>Agenda Tutorías</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px] pb-8">Agenda Tutorías</p>
                     <a href="#">
-                        <img src="<?= BASE_URL ?>public/img/calendar.png" class="w-40 h-48 mb-2" alt="Tutorías">
+                        <img src="<?= BASE_URL ?>public/img/calendar.png" class="w-40 h-48 sm:w-40 sm:h-48 md:w-48 md:h-56 lg:w-60 lg:h-64 xl:w-71 xl:h-80 mb-2 transition-transform duration-300 hover:scale-95"
+                            alt="Tutorías">
                     </a>
                 </div>
             </div>
@@ -58,21 +54,21 @@
             </h2>
 
             <!-- Tarjetas de áreas -->
-            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 px-6">
+            <div class="font-bold font-sans grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 px-6 ">
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
                     <a href="#">
                         <img src="<?= BASE_URL ?>public/img/database.png" class="w-32 h-40 mx-auto mb-2"
                             alt="Bases de Datos">
                     </a>
-                    <p>Bases de Datos</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px]">Bases de Datos</p>
                 </div>
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
                     <a href="#">
                         <img src="<?= BASE_URL ?>public/img/math.png" class="w-32 h-40 mx-auto mb-2" alt="Matemáticas">
                     </a>
-                    <p>Matemáticas</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px]">Matemáticas</p>
                 </div>
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
@@ -80,14 +76,14 @@
                         <img src="<?= BASE_URL ?>public/img/programming.png" class="w-32 h-40 mx-auto mb-2"
                             alt="Programación Teórica">
                     </a>
-                    <p>Programación Teórica</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px]">Programación Teórica</p>
                 </div>
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
                     <a href="#">
                         <img src="<?= BASE_URL ?>public/img/physic.png" class="w-32 h-40 mx-auto mb-2" alt="Física">
                     </a>
-                    <p>Física</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px]">Física</p>
                 </div>
                 <div
                     class="bg-white/10 backdrop-blur-sm p-4 rounded-lg text-center hover:scale-105 transition-transform">
@@ -95,11 +91,12 @@
                         <img src="<?= BASE_URL ?>public/img/programming2.png" class="w-32 h-40 mx-auto mb-2"
                             alt="Programación Práctica">
                     </a>
-                    <p>Programación Práctica</p>
+                    <p class="text-[20px] xl:text-[25px] lg:text-[20px] md:text-[20px]">Programación Práctica</p>
                 </div>
             </div>
         </main>
     </div>
+    
 </body>
 
 </html>
--- view/layouts/header.php
+++ view/layouts/header.php
@@ -1,16 +1,16 @@
 <!-- Header -->
 <header class="flex flex-col sm:flex-row justify-between items-center px-6 py-1 text-white" style="background-color: #803CB9;">
-    <div>
-    <img src="<?= BASE_URL ?>public/img/logo.png" alt="Logo cloUD" class="h-12 sm:h-14">
+    <div >
+    <img src="<?= BASE_URL ?>public/img/logo.png" alt="Logo cloUD" class="h-12 sm:h-14 lg:h-16 xl:h-20 pl-5">
 </div>
-    <nav class="flex space-x-10 mt-3 sm:mt-0">
-    <a href="<?= BASE_URL ?>index.php?url=RouteController/materials" class="hover:underline">Gestiona Materiales Académicos</a>
-    <a href="#" class="hover:underline">Agenda Tutorías</a>
-    <a href="#" class="hover:underline">FAQ</a>
+    <nav class="flex space-x-4 sm:space-x-10 lg:space-x-32 xl:space-x-80 mt-3 sm:mt-0 font-semibold font-sans">
+    <ul class="transition-transform duration-300 hover:scale-110 text-center transition-all duration-200" ><a href="<?= BASE_URL ?>index.php?url=RouteController/materials" class="text-[15px] lg:text-[20px] xl:text-[25px] text-center custom-underline">Gestiona Materiales Académicos</a></ul>
+    <ul class="transition-transform duration-300 hover:scale-110 text-center transition-all duration-200"><a href="#" class="text-[15px] lg:text-[20px] xl:text-[25px] text-center custom-underline">Agenda Tutorías</a></ul>
+    <ul class="transition-transform duration-300 hover:scale-110 text-center transition-all duration-200"><a href="#" class="text-[15px] lg:text-[20px] xl:text-[25px] text-center custom-underline">FAQ</a></ul>
     </nav>
     <div>
-    <a href="#">
+    <ul><a href="#">
         <img src="<?= BASE_URL ?>public/img/login.png" alt="Logo login" class="h-12 sm:h-14">
-    </a>
-</div>
+    </a></ul>
+    </div>
 </header>
